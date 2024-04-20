e04d16c5f4f03a347378298a6f741f8594b07ada
Kartikey <kartikeyguptaakgec@gmail.com>
2024-04-20 18:42:46 +0530

    First code push for Github-Jenkins Pipeline

---
+++ Tests/test_Login.py
@@ -0,0 +1,32 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+
+
+class Test_Login():
+
+    def test_validLogin_scenario(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.rahulshettyacademy.com/locatorspractice/")
+        driver.maximize_window()
+        driver.find_element(By.XPATH,"//input[@id='inputUsername']").send_keys("KarryBoy")
+        driver.find_element(By.XPATH,"//input[@name='inputPassword']").send_keys("rahulshettyacademy")
+        driver.find_element(By.XPATH,"//button[text()='Sign In']").click()
+        time.sleep(2)
+        succesMessage = driver.find_element(By.XPATH,"//p[text()='You are successfully logged in.']").text
+        assert succesMessage.__eq__("You are successfully logged in.")
+        driver.close()
+
+    def test_invalidLogin_scenario(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.rahulshettyacademy.com/locatorspractice/")
+        driver.maximize_window()
+        driver.find_element(By.XPATH, "//input[@id='inputUsername']").send_keys("KarryBoy")
+        driver.find_element(By.XPATH, "//input[@name='inputPassword']").send_keys("rahulshettyacademy123")
+        driver.find_element(By.XPATH, "//button[text()='Sign In']").click()
+        time.sleep(2)
+        succesMessage = driver.find_element(By.XPATH, "//p[text()='* Incorrect username or password']").text
+        assert succesMessage.__eq__("* Incorrect username or password123")
+        driver.close()
