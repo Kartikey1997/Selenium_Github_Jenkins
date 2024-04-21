5c9175c08d034292ecbfcadf9097c563a2ef099a
Kartikey1997 <91788415+Kartikey1997@users.noreply.github.com>
2024-04-21 11:02:37 +0530

    Update test_Login.py

--- Tests/test_Login.py
+++ Tests/test_Login.py
@@ -9,7 +9,13 @@ import pytest
 class Test_Login():
 
     def test_validLogin_scenario(self):
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        options.add_argument('--disable-infobars')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--remote-debugging-port=9222')
+        driver = webdriver.Chrome(options=options,service=Service(ChromeDriverManager().install()))
         driver.get("https://www.rahulshettyacademy.com/locatorspractice/")
         driver.maximize_window()
         driver.find_element(By.XPATH,"//input[@id='inputUsername']").send_keys("KarryBoy")
@@ -21,7 +27,13 @@ class Test_Login():
         driver.close()
 
     def test_invalidLogin_scenario(self):
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        options.add_argument('--disable-infobars')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--remote-debugging-port=9222')
+        driver = webdriver.Chrome(options=options,service=Service(ChromeDriverManager().install()))
         driver.get("https://www.rahulshettyacademy.com/locatorspractice/")
         driver.maximize_window()
         driver.find_element(By.XPATH, "//input[@id='inputUsername']").send_keys("KarryBoy")
